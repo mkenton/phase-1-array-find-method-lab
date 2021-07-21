5ec886a9fd463437e967064d90aa84a2f565a449
Michael <mkenton2020@icloud.com>
2021-07-21 12:54:41 -0500

    commit tree 0744542ae83f21fb5d6c673569b353fb6784deec

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+// function findW(game){
+//     return game.result === "W"
+// }
+
+// function superbowlWin(array){
+//     const record = array.find(findW)
+//     return record ? record.year : undefined;
+// }
+
+function superbowlWin(array){
+    const record = array.find(game => game.result === "W")
+    return record ? record.year : undefined;
+}
